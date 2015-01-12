7d74ae253d834e571c5203a2f401cc03161e824a
anorneto <anorneto@outlook.com>
2015-01-12 02:23:03 -0200

    Changed the .cpp to add the other send functions

--- attiny85_ir_send.cpp
+++ attiny85_ir_send.cpp
@@ -12,10 +12,86 @@
  * For details, see http://blog.tkjelectronics.dk
  * Attiny85 adaptation by Nick Gammon http://arduino.cc/forum/index.php?topic=91488.msg687101#msg687101)
  * Spilt into two lib by philteta@gmail.com www.tetalab.org (IRTinyTX-lib-v1)
+ *
+ *Modified by Anor Neto to send all types of ir codes with attiny
+ *  January 20015  https://github.com/anorneto/attiny85_ir_send
  */
 
-#include "attiny85_RC6_IR.h"
+#include "attiny85_ir_send.h"
 
+void IRsend::sendRaw(unsigned int buf[], int len, int hz)
+{
+  enableIROut(hz);
+  for (int i = 0; i < len; i++) {
+    if (i & 1) {
+      space(buf[i]);
+    } 
+    else {
+      mark(buf[i]);
+    }
+  }
+  space(0); // Just to be sure
+}
+
+void IRsend::sendNEC(unsigned long data, int nbits)
+{
+  enableIROut(38);
+  mark(NEC_HDR_MARK);
+  space(NEC_HDR_SPACE);
+  for (int i = 0; i < nbits; i++) {
+    if (data & TOPBIT) {
+      mark(NEC_BIT_MARK);
+      space(NEC_ONE_SPACE);
+    } 
+    else {
+      mark(NEC_BIT_MARK);
+      space(NEC_ZERO_SPACE);
+    }
+    data <<= 1;
+  }
+  mark(NEC_BIT_MARK);
+  space(0);
+}
+
+void IRsend::sendSony(unsigned long data, int nbits) {
+  enableIROut(40);
+  mark(SONY_HDR_MARK);
+  space(SONY_HDR_SPACE);
+  data = data << (32 - nbits);
+  for (int i = 0; i < nbits; i++) {
+    if (data & TOPBIT) {
+      mark(SONY_ONE_MARK);
+      space(SONY_HDR_SPACE);
+    } 
+    else {
+      mark(SONY_ZERO_MARK);
+      space(SONY_HDR_SPACE);
+    }
+    data <<= 1;
+  }
+}
+
+// Note: first bit must be a one (start bit)
+void IRsend::sendRC5(unsigned long data, int nbits)
+{
+  enableIROut(36);
+  data = data << (32 - nbits);
+  mark(RC5_T1); // First start bit
+  space(RC5_T1); // Second start bit
+  mark(RC5_T1); // Second start bit
+  for (int i = 0; i < nbits; i++) {
+    if (data & TOPBIT) {
+      space(RC5_T1); // 1 is space, then mark
+      mark(RC5_T1);
+    } 
+    else {
+      mark(RC5_T1);
+      space(RC5_T1);
+    }
+    data <<= 1;
+  }
+  space(0); // Turn off at end
+}
 
 void IRsend::sendRC6(unsigned long data, int nbits)
 {
@@ -46,6 +122,107 @@ void IRsend::sendRC6(unsigned long data, int nbits)
     data <<= 1;
   }
   space(0); // Turn off at end
+} 
+
+void IRsend::sendSAMSUNG(unsigned long data, int nbits)
+{
+  enableIROut(38);
+  mark(SAMSUNG_HDR_MARK);
+  space(SAMSUNG_HDR_SPACE);
+  for (int i = 0; i < nbits; i++) {
+    if (data & TOPBIT) {
+      mark(SAMSUNG_BIT_MARK);
+      space(SAMSUNG_ONE_SPACE);
+    } 
+    else {
+      mark(SAMSUNG_BIT_MARK);
+      space(SAMSUNG_ZERO_SPACE);
+    }
+    data <<= 1;
+  }
+  mark(SAMSUNG_BIT_MARK);
+  space(0);
+}
+
+void IRsend::sendSharpRaw(unsigned long data, int nbits) {
+  enableIROut(38);
+
+  // Sending codes in bursts of 3 (normal, inverted, normal) makes transmission
+  // much more reliable. That's the exact behaviour of CD-S6470 remote control.
+  for (int n = 0; n < 3; n++) {
+    for (int i = 1 << (nbits-1); i > 0; i>>=1) {
+      if (data & i) {
+        mark(SHARP_BIT_MARK);
+        space(SHARP_ONE_SPACE);
+      }
+      else {
+        mark(SHARP_BIT_MARK);
+        space(SHARP_ZERO_SPACE);
+      }
+    }
+    
+    mark(SHARP_BIT_MARK);
+    space(SHARP_ZERO_SPACE);
+    delay(40);
+
+    data = data ^ SHARP_TOGGLE_MASK;
+  }
+}
+
+// Sharp send compatible with data obtained through decodeSharp
+void IRsend::sendSharp(unsigned int address, unsigned int command) {
+  sendSharpRaw((address << 10) | (command << 2) | 2, 15);
+}
+
+void IRsend::sendPanasonic(unsigned int address, unsigned long data) {
+    enableIROut(35);
+    mark(PANASONIC_HDR_MARK);
+    space(PANASONIC_HDR_SPACE);
+    
+    for(int i=0;i<16;i++)
+    {
+        mark(PANASONIC_BIT_MARK);
+        if (address & 0x8000) {
+            space(PANASONIC_ONE_SPACE);
+        } else {
+            space(PANASONIC_ZERO_SPACE);
+        }
+        address <<= 1;        
+    }    
+    for (int i=0; i < 32; i++) {
+        mark(PANASONIC_BIT_MARK);
+        if (data & TOPBIT) {
+            space(PANASONIC_ONE_SPACE);
+        } else {
+            space(PANASONIC_ZERO_SPACE);
+        }
+        data <<= 1;
+    }
+    mark(PANASONIC_BIT_MARK);
+    space(0);
+}
+
+void IRsend::sendJVC(unsigned long data, int nbits, int repeat)
+{
+    enableIROut(38);
+    data = data << (32 - nbits);
+    if (!repeat){
+        mark(JVC_HDR_MARK);
+        space(JVC_HDR_SPACE); 
+    }
+    for (int i = 0; i < nbits; i++) {
+        if (data & TOPBIT) {
+            mark(JVC_BIT_MARK);
+            space(JVC_ONE_SPACE); 
+        } 
+        else {
+            mark(JVC_BIT_MARK);
+            space(JVC_ZERO_SPACE); 
+        }
+        data <<= 1;
+    }
+    mark(JVC_BIT_MARK);
+    space(0);
 }
 
 void IRsend::mark(int16_t time) {
@@ -75,10 +252,7 @@ void IRsend::enableIROut(uint8_t khz) {
   // to make sure it gives reasonable results.  (Watch out for overflow / underflow / rounding.)
   // TIMER0 is used in phase-correct PWM mode, with OCR0A controlling the frequency and OCR0B
   // controlling the duty cycle.
-  // There is no prescaling, so the output frequency is 16MHz / (2 * OCR0A)
-  // To turn the output on and off, we leave the PWM running, but connect and disconnect the output pin.
-  // A few hours staring at the ATmega documentation and this will all make sense.
-  // See my Secrets of Arduino PWM at http://arcfn.com/2009/07/secrets-of-arduino-pwm.html for details.
+  // There is no prescaling, so the output frequency is F_CPU/ (2 * OCR0A)
   
   DDRB |= _BV(IRLED); // Set as output
 
